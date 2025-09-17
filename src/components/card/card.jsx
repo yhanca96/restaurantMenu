150963e92734c06dc700b5d5cdecaf7c66dfdc85
yhanca96 <yhann1116816@correo.itm.edu.co>
2025-09-17 10:30:02 -0500

    card component

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,23 @@
 import style from "./card.module.css";
-function Card(image, category, name, price) {
+export default function Card({ image, category, name, price }) {
   return (
-    <div>
-      <h1>hola</h1>
-      <h2>hola2</h2>
+    <div className={style.principalCard}>
+      <div className={style.headerCard}>
+        <img className={style.imageCard} src={image} alt="product menu" />
+        <button className={style.addCardButton}>
+          <img
+            src="../../../assets/images/icon-add-to-cart.svg"
+            alt="add to cart"
+          />
+          Add to Cart
+        </button>
+      </div>
+
+      <div className={style.bodyCard}>
+        <p className={style.bodyCategory}>{category}</p>
+        <p className={style.bodyName}>{name}</p>
+        <p className={style.bodyPrice}>${price}</p>
+      </div>
     </div>
   );
 }
